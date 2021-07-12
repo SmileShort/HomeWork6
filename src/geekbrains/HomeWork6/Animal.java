a4c2fa8bc4cd9e62effb46985e586a5cc015cb18
SmileShort <86470906+SmileShort@users.noreply.github.com>
2021-07-12 16:55:16 +0500

    HomeWork6!

--- src/geekbrains/HomeWork6/Animal.java
+++ src/geekbrains/HomeWork6/Animal.java
@@ -5,12 +5,10 @@ public class Animal {
     protected String name;
     protected String color;
     protected int distance;
-    int sumAnimal = 0;
 
     Animal (String name, String color){
         this.name = name;
         this.color = color;
-        sumAnimal = sumAnimal + 1;
     }
 
       void run (int distance){
